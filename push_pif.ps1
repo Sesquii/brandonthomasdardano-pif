eaaab1a0db621913c60f670c0703f7f8849765b5
Brandon Thomas Dardano <dardanobrandon@gmail.com>
2025-05-17 09:17:34 -0500

    should be final update before 2. was just a minor error on initialization vector

--- push_pif.ps1
+++ push_pif.ps1
@@ -24,7 +24,7 @@ function Encrypt-String($plainText, $key) {
     $IV = New-Object byte[] 16
     [System.Security.Cryptography.RandomNumberGenerator]::Create().GetBytes($IV)
 
-    $aes.IV = $IV
+    $aes.IV = $IV[0..15] # Ensure IV is exactly 16 bytes
     $encryptor = $aes.CreateEncryptor()
     $encrypted = $encryptor.TransformFinalBlock($bytes, 0, $bytes.Length)
 
@@ -43,7 +43,7 @@ function Decrypt-String($encryptedData, $key) {
 
     $aes = New-Object System.Security.Cryptography.AesManaged
     $aes.Key = [System.Text.Encoding]::UTF8.GetBytes($key.PadRight(32).Substring(0,32))
-    $aes.IV = $IV
+    $aes.IV = $IV[0..15] # Ensure IV is exactly 16 bytes
 
     $decryptor = $aes.CreateDecryptor()
     $decrypted = $decryptor.TransformFinalBlock($encryptedText, 0, $encryptedText.Length)
